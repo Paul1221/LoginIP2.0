690b99abbd6d51068d5f4b881125214d35ae342c
Paul1221 <paul_man70@yahoo.com>
2020-05-03 23:27:30 +0300

    modificat baza de date + functii pt integrare

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,8 @@ import { RegisterComponent } from './register';
 import { RecuperareComponent } from './recuperare'
 import { AlertComponent } from './_components';;
 import { SocialLoginModule,AuthServiceConfig,FacebookLoginProvider,GoogleLoginProvider } from 'angularx-social-login';;
+import { ProfileCreationComponent } from './profile-creation/profile-creation.component';
+
 
 const config=new AuthServiceConfig([{
     id: GoogleLoginProvider.PROVIDER_ID,
@@ -41,7 +43,8 @@ export function provideConfig(){
         ReactiveFormsModule,
         HttpClientModule,
         appRoutingModule,
-        SocialLoginModule
+        SocialLoginModule,
+        
     ],
     declarations: [
         AppComponent,
@@ -50,7 +53,9 @@ export function provideConfig(){
         LoginComponent,
         RegisterComponent,
         RecuperareComponent,
-        AlertComponent],
+        AlertComponent,
+        ProfileCreationComponent],
+        
     providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,13 +5,14 @@ import { LoginComponent } from './login';
 import { RegisterComponent } from './register';
 import { RecuperareComponent } from './recuperare'
 import { AuthGuard } from './_helpers';
+import { ProfileCreationComponent } from './profile-creation/profile-creation.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: 'recuperare', component: RecuperareComponent },
-
+    { path: 'profileCreation', component: ProfileCreationComponent },
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
 ];
